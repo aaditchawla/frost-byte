3523549cbbfa568d703ad53bf70f0f1636c11797
rtalwar05 <talwarrhea@gmail.com>
2026-01-17 15:49:33 -0500

    tailwind css installed

--- src/app/frontend/page.tsx
+++ src/app/frontend/page.tsx
@@ -2,13 +2,11 @@ import React from "react";
 
 export default function HomePage() {
   return (
-    <html>
-      <>
-        <h1>Welcome to the Home Page</h1>
-        <body>
-          <p>This is the main landing page of the application.</p>
-        </body>
-      </>
-    </html>
+    <main>
+      <h1>Welcome to the Home Page</h1>
+      <div className="border p-4 text-center text-[#245693]">
+        This is the main landing page of the application.
+      </div>
+    </main>
   );
 }
