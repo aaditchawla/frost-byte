1284f64d4ff4412ce5521787972aa55edaeb65e8
rtalwar05 <talwarrhea@gmail.com>
2026-01-17 18:17:27 -0500

    search inputs

--- src/app/frontend/page.tsx
+++ src/app/frontend/page.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import MapComponent from "./components/mapComponent";
 
 export default function HomePage() {
   return (
     <main className="min-h-screen">
-      <h1 className="font-molle text-6xl font-bold text-blue-300 p-12">
+      <h1 className="font-molle text-5xl font-bold text-blue-300 pt-12 px-12">
         Frostbyte
       </h1>
+      <div>
+        <MapComponent />
+      </div>
     </main>
   );
 }
